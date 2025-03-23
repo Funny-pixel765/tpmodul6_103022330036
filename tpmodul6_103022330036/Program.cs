146eb2305dbd138ef36a6224f5840d34c335229f
Funny-pixel765 <funnyrnd92@gmail.com>
2025-03-23 21:35:19 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_103022330036/Program.cs
+++ tpmodul6_103022330036/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-h
+﻿using System;
+namespace tpmodul6_103022330036
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaTubeVideo video1 = new SayaTubeVideo(1, "Tutorial Design By Contract-- Tio Funny", 0);
+            video1.increasePlayCount(100);
+            Console.WriteLine();
+            video1.increasePlayCount(10000);
+            video1.printVideoDetails();
+
+        }
+    }
+}
---
+++ tpmodul6_103022330036/SayaTubeVideo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_103022330036
+{
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+        Random random = new Random();
+
+        public SayaTubeVideo(int id, string title, int playCount)
+        {
+            this.id = random.Next(1000, 9999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void increasePlayCount(int view)
+        {
+            this.playCount += view;
+        }
+
+        public void printVideoDetails()
+        {
+            Console.WriteLine("ID: " + this.id);
+            Console.WriteLine("Title: " + this.title);
+            Console.WriteLine("Jumlah views: " + this.playCount);
+        }
+    }
+}
+
+        
