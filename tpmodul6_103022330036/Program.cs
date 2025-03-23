839b44c8d83521cb6e879d71f44adf5173208ac3
Funny-pixel765 <funnyrnd92@gmail.com>
2025-03-23 22:27:34 +0700

    implementasi design by contract

--- tpmodul6_103022330036/Program.cs
+++ tpmodul6_103022330036/Program.cs
@@ -5,12 +5,26 @@ namespace tpmodul6_103022330036
     {
         static void Main(string[] args)
         {
-            SayaTubeVideo video1 = new SayaTubeVideo(1, "Tutorial Design By Contract-- Tio Funny", 0);
-            video1.increasePlayCount(100);
-            Console.WriteLine();
-            video1.increasePlayCount(10000);
-            video1.printVideoDetails();
-
+            try
+            {
+                SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design By Contract-- Tio Funny");
+                video1.printVideoDetails();
+                Console.WriteLine();
+                for (int i = 0; i < 1000; i++) //perulangan untuk menambahkan jumlah view sebanyak 1000
+                {
+                    video1.increasePlayCount(1000000);
+                    video1.printVideoDetails();
+                    Console.WriteLine();
+                    if (video1.getPlayCount() == int.MaxValue) // berfungsi membatasi perulangan jika jumlah view sudah dibatas maksimum
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Terjadi error! " + e.Message);
+            }
         }
     }
 }
--- tpmodul6_103022330036/SayaTubeVideo.cs
+++ tpmodul6_103022330036/SayaTubeVideo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,27 +13,58 @@ namespace tpmodul6_103022330036
         private int id;
         private string title;
         private int playCount;
-        Random random = new Random();
+        Random random = new Random(); // random untuk menginisiasikan id video
 
-        public SayaTubeVideo(int id, string title, int playCount)
+        public SayaTubeVideo(string title) // constructor untuk input title dan inisiasi id serta playcount
         {
-            this.id = random.Next(1000, 9999);
+            
+            Debug.Assert(title != null, "Judul tidak boleh kosong");
+            Debug.Assert(title.Length <= 100, "Judul tidak boleh lebih dari 100 kata");
+             
+            this.id = random.Next(10000, 99999); // disini menginput dengan random number
             this.title = title;
-            this.playCount = 0;
+            this.playCount = 0; //playcount awal mulai dari 0
         }
 
-        public void increasePlayCount(int view)
+        public void increasePlayCount(int views)
         {
-            this.playCount += view;
+            // Method untuk menambah jumlah views dan tidak lebih dari ketentuan yang sudah diberikan
+            Debug.Assert(views < 10000000, "Jumlah view harus lebih dari 0 tetapi tidak lebih dari 10.000.000");
+
+            try
+            {
+                checked // jika terjadi overflow maka akan masuk ke exception
+                {
+                    if (this.playCount > int.MaxValue - views)
+                    {
+                        
+                        throw new OverflowException(); //jika exception playcount melebihi batas maksimum int
+                    }
+                    this.playCount += views; //menambahkan views ke playcount
+                }
+            }
+            // exception untuk menghandle overflow jika melebihi batas maksimum int
+
+            catch (OverflowException)
+            {
+                Console.WriteLine("Terjadi Error! sudah melebihi batas maksimum views");
+                // jika melebihi batas maka playcount akan di set ke nilai maksimum int
+                this.playCount = int.MaxValue;
+            }
         }
 
         public void printVideoDetails()
         {
-            Console.WriteLine("ID: " + this.id);
-            Console.WriteLine("Title: " + this.title);
-            Console.WriteLine("Jumlah views: " + this.playCount);
+            // Method disini untuk menampilkan detail video
+            Console.WriteLine("ID : " + this.id);
+            Console.WriteLine("Judul Video: " + this.title);
+            Console.WriteLine("Jumlah Views: " + this.playCount);
+        }
+
+        public int getPlayCount()
+        {
+            // getter untuk mendapatkan jumlah playcount
+            return this.playCount;
         }
     }
 }
-
-        
